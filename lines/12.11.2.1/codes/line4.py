34c6546d5648045b8ec964a1df9f86561a14cbde
likhith1101 <68158356+likhith1101@users.noreply.github.com>
2023-01-21 20:06:41 +0530

    Add files via upload

--- lines/12.11.2.1/codes/line4.py
+++ lines/12.11.2.1/codes/line4.py
@@ -5,19 +5,12 @@ import math as ma
 A = np.array([12/13, -(3/13), -(4/13)])
 B  = np.array([4/13,12/13,3/13])
 C  = np.array([3/13,-(4/13),12/13])
-# Find the magnitude of the vector
+P=np.array([A,B,C])
+I=[[1,0,0],[0,1,0],[0,0,1]]
+R=(P.T)@P
+if int(R[0][0]) and int(R[1][1]) and int(R[2][2])==1:
+    print('All three vectors are orthogonal to each other')
+else:
+    print('Not orthogonal')
 
-a = np.linalg.norm(A)
-b = np.linalg.norm(B)
-c = np.linalg.norm(C)
-
-# Divide the vector by its magnitude to find the direction cosines
-dir_cos1 = ((A.T)@B) / (a*b)
-dir_cos2 = ((B.T)@C) / (b*c)
-dir_cos3 = ((A.T)@C) / (a*c)
-
-theta1=ma.acos(dir_cos1)*(180.0/ma.pi)
-theta2=ma.acos(dir_cos2)*(180.0/ma.pi)
-theta3=ma.acos(dir_cos3)*(180.0/ma.pi)
-print(theta1,theta2,theta3)
 
