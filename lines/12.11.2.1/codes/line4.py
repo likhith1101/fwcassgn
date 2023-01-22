f8f04e27b8868c0dc20224a41a22baafc364541e
likhith1101 <68158356+likhith1101@users.noreply.github.com>
2023-01-22 13:57:36 +0530

    Add files via upload

--- lines/12.11.2.1/codes/line4.py
+++ lines/12.11.2.1/codes/line4.py
@@ -2,13 +2,14 @@ import numpy as np
 import math as ma
 
 # Define the vector
-A = np.array([12/13, -(3/13), -(4/13)])
-B  = np.array([4/13,12/13,3/13])
-C  = np.array([3/13,-(4/13),12/13])
+A = np.array(np.round_([(12/13), ((-3)/13), ((-4)/13)]))
+B  = np.array(np.round_([(4/13),(12/13),(3/13)]))
+C  = np.array(np.round_([(3/13),((-4)/13),(12/13)]))
 P=np.array([A,B,C])
-I=[[1,0,0],[0,1,0],[0,0,1]]
-R=(P.T)@P
-if int(R[0][0]) and int(R[1][1]) and int(R[2][2])==1:
+I=(np.eye(3, dtype = float))
+print((P.T)@P)
+
+if ((P.T)@P==I).all():
     print('All three vectors are orthogonal to each other')
 else:
     print('Not orthogonal')
