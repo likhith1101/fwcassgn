75826ef789c25d244a2a6472de19e825af350cc0
likhith1101 <68158356+likhith1101@users.noreply.github.com>
2023-01-23 12:22:43 +0530

    Add files via upload

--- lines/12.11.2.1/codes/line4.py
+++ lines/12.11.2.1/codes/line4.py
@@ -7,8 +7,9 @@ B  = np.array(np.round_([(4/13),(12/13),(3/13)]))
 C  = np.array(np.round_([(3/13),((-4)/13),(12/13)]))
 P=np.array([A,B,C])
 I=(np.eye(3, dtype = float))
+print((P.T)@P)
 
-if ((P.T)@P==I).all():
+if (P@(P.T)==I).all() and  ((P.T)@P==I).all():
     print('All three vectors are orthogonal to each other')
 else:
     print('Not orthogonal')
