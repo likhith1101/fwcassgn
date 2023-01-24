2220f589c55496ec779547f9091593088803b612
likhith1101 <68158356+likhith1101@users.noreply.github.com>
2023-01-24 08:44:53 +0530

    Update line5.py

--- lines/12.11.3.1/codes/line5.py
+++ lines/12.11.3.1/codes/line5.py
@@ -1,7 +1,7 @@
 import numpy as np
 n=[]
 for i in range(3):
-	n.append(int(input('enter array elements:')))
+	n.append(int(input('enter coefficients of equation:')))
 n=np.array(n)
 c=int(input('enter constant value:'))
 magn=np.linalg.norm(n)
