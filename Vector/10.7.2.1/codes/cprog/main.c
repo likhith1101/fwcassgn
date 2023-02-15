562da46b9efd371fc92ee1fc28ce53e247063265
likhith1101 <68158356+likhith1101@users.noreply.github.com>
2023-02-15 12:24:10 +0530

    Add files via upload

--- Vector/10.7.2.1/codes/cprog/main.c
+++ Vector/10.7.2.1/codes/cprog/main.c
@@ -8,13 +8,13 @@ Find the coordinates of the point which divides the join (−1,7) and (4,−3) i
 #include"lib.h"			//Functions
 int main()                 
 {
-	double **p,**q,**r;
-	double ratio;		//initializing the variables as matrices
+	double **p,**q,**r;		//initializing the variables
+	double ratio;	 
 	int m=2,n=1;
-	p=loadtxt("p.dat",2,1);		//loading the point A from the text file
-	q=loadtxt("q.dat",2,1);	        //loading the point B from the text file
-	ratio=1.5;
-	r=scalar_mul(linalg_add(q,scalar_mul(p,m,n,ratio),m,n),m,n,1/(1+ratio));
+	p=loadtxt("p.dat",2,1);		//loading the point P from the text file
+	q=loadtxt("q.dat",2,1);	        //loading the point Q from the text file
+	ratio=1.5;			//ratio is given 3/2
+	r=scalar_mul(linalg_add(q,scalar_mul(p,m,n,ratio),m,n),m,n,1/(1+ratio)); //finding R points using Section formula
 	print(r,m,n);			//printing the result
 	save(r,m,n);		//saving the result to the figure
 }
