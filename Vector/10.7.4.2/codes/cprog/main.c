c1762bfcb820bad1dff638b74ee0ef9e23cf3d31
likhith1101 <68158356+likhith1101@users.noreply.github.com>
2023-02-16 23:32:29 +0530

    Update main.c

--- Vector/10.7.4.2/codes/cprog/main.c
+++ Vector/10.7.4.2/codes/cprog/main.c
@@ -1,5 +1,8 @@
 /*Code by G. Likhitheshwar (works on termux)
 Feb 16, 2023
+Find a relation between x and y if the points (x, y), (1, 2)  and  (7, 0) are collinear.
+Code is free any one can use 
+https://github.com/likhith1101/fwcassgn/blob/main/LICENSE.md
 */
  
 #include<stdio.h>       
@@ -9,20 +12,19 @@ Feb 16, 2023
 int main()                 
 {
 	double **a,**b,**c,**d,**e,**f,**dt,**et;		//initializing the variables
-	double area1;
 	int r;
 	int m=2,n=1;
-	a=loadtxt("a.dat",2,1);	//loading the point A from the text file
-	b=loadtxt("b.dat",2,1);	//loading th    e point B from the text file 
-	c=loadtxt("c.dat",2,1);//loading th    e point C from the text file
+	a=loadtxt("a.dat",2,1);	 //loading the point A from the text file
+	b=loadtxt("b.dat",2,1);	//loading the point B from the text file 
+	c=loadtxt("c.dat",2,1);//loading the point C from the text file
 	d=linalg_sub(a,b,m,n); //subtraction of two matrices A and B
-	e=linalg_sub(a,c,m,n);//subtraction     of two matrices A and B
-	dt=transpose(d,m,n);//transpose of matrix D
-	et=transpose(e,m,n);//transpose of matrix E
-	f=concat_two(dt,et);//concatentaing two matrices into a single matrix F
-	r=rank(2,2,f);//Finding the rank of matrix F
-	save_r(r);//saving the result to t    he figure
-	if(r==1) //check whether rank of matrix is equal to 1 or not
+	e=linalg_sub(a,c,m,n);//subtraction of two matrices A and B
+	dt=transpose(d,m,n); //transpose of matrix D
+	et=transpose(e,m,n); //transpose of matrix E
+	f=concat_two(dt,et); //concatentaing two matrices into a single matrix F
+	r=rank(2,2,f);      //Finding the rank of matrix F
+	save_r(r);          //saving the result to the figure
+	if(r==1)            //check whether rank of matrix is equal to 1 or not
 	{
 		printf("collinear because rank(F) is one");
 	}
