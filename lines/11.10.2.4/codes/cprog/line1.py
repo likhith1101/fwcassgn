34df0f12dbbd4ceaf3690dfbcc073113ffad3279
likhith1101 <68158356+likhith1101@users.noreply.github.com>
2023-02-17 12:46:53 +0530

    Add files via upload

---
+++ lines/11.10.2.4/codes/cprog/line1.py
@@ -0,0 +1,70 @@
+
+#Python libraries for math and graphics
+import numpy as np
+import mpmath as mp
+import math as ma
+import matplotlib.pyplot as plt
+from numpy import linalg as LA
+
+
+#if using termux
+import subprocess
+import shlex
+#end if
+
+def line_dir_pt(m,A,k1,k2):
+  len = 10
+  dim = A.shape[0]
+  x_AB = np.zeros((dim,len))
+  lam_1 = np.linspace(k1,k2,len)
+  for i in range(len):
+    temp1 = A + lam_1[i]*m
+    x_AB[:,i]= temp1.T
+  return x_AB       
+
+
+#Input parameters
+A = np.loadtxt('a.dat',dtype='float') 
+m = np.loadtxt('result.dat',dtype='float')
+
+
+
+
+
+
+
+
+##Generating the line 
+x_mA = line_dir_pt(m,A,-8,3)
+
+
+
+#Plotting all lines
+plt.plot(x_mA[0,:],x_mA[1,:],label='Line equation')
+
+
+
+
+#Labeling the coordinates
+tri_coords = np.vstack((A,)).T
+plt.scatter(tri_coords[0,:], tri_coords[1,:])
+vert_labels = ['A']
+for i, txt in enumerate(vert_labels):
+    plt.annotate(txt, # this is the text
+                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid() # minor
+plt.axis('equal')
+
+#if using termux
+plt.savefig('/sdcard/download/fwcassgn/trunk/fwcassgn/trunk/line/11.10.2.4/figs/line1.png')
+#subprocess.run(shlex.split("termux-open /sdcard/download/fwcassgn/trunk/fwcassgn/trunk/line/11.10.2.4/figs/line.png"))
+#else
+plt.show()               
+
---
+++ lines/11.10.2.4/codes/cprog/main.c
@@ -0,0 +1,23 @@
+/*Code by G. Likhitheshwar (works on termux)
+Feb 17, 2023
+Passing through (2,3.46) and inclined with the x-axis at an angle of 75 deg.
+*/
+ 
+#include<stdio.h>       
+#include<stdlib.h>
+#include<math.h>
+#include"lib.h"				//Functions
+int main()                 
+{
+	double **a,**z,**m,**n;		//initializing the variables
+	double theta;
+	int M=2,N=1;
+	a=loadtxt("a.dat",2,1);		//loading the point A from the text file
+	theta=radians(75.0);   		//Store 75deg in radians form
+	m=dir_vec(cos(theta),sin(theta));	//Direction vector m
+	save(m,M,N); 			//saving the result to the figure
+	z=loadtxt("z.dat",2,2);		//loading the point z from the text file
+	n=matmul(z,m,M,2,2);		//Matrix multiplication of two matrices
+	print(n,M,N);			//printing the normal vector
+	
+}
