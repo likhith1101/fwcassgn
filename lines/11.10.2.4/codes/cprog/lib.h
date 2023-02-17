fea4f9f41fdf478086da3de9b1718ad0fe680296
likhith1101 <68158356+likhith1101@users.noreply.github.com>
2023-02-17 12:57:22 +0530

    Update lib.h

--- lines/11.10.2.4/codes/cprog/lib.h
+++ lines/11.10.2.4/codes/cprog/lib.h
@@ -4,10 +4,6 @@ double **loadtxt(char *str,int m,int n);
 double linalg_norm(double **a, int m);
 double **linalg_sub(double **a, double **b, int m, int n);
 double **linalg_add(double **a, double **b, int m, int n);
-double **scalar_mul(double **A,int row,int col,double num);
-double cross_prod(double **a,double **b);
-double **concat_two(double **a,double **b);
-int rank(int r1, int c1,double **a);
 double **transpose(double **a,  int m, int n);
 double **createMat1(int m,int n);
 double **dir_vec(double a,double b);
@@ -196,87 +192,13 @@ void print(double **p, int m,int n)
 }
 //End function for printing
 
-double **scalar_mul(double **A,int row,int col,double num)
-{
-int i,j;
- for(i=0; i<row; i++)
-    {
-        for(j=0; j<col; j++)
-        {
-            /* (cAij) = c . Aij */
-            A[i][j] = num * A[i][j];
-        }
-    }
-    return A;
-}
 
-double **concat_two(double **a,double **b)
-{
-	double **f;
-	int m=2;
-	int n=2;
-	f=createMat(m,n);
-	f[0][0]=a[0][0];
-	f[0][1]=a[0][1];
-	f[1][0]=b[0][0];
-	f[1][1]=b[0][1];
-	return f;
-}
 
-void swap(int r1, int r2, int c,double **a)
-{
-   int t;
-   int i,j;
-   for(i=0;i<c;i++)
-   {
-      t= a[r1][i];
-      a[r1][i] = a[r2][i];
-      a[r2][i]=t;
-   }
-}
-	
-int rank(int r1, int c1,double **a)
-{
-    int i,j,k;
-    float ratio;
-    for(i=0;i<c1;i++)
-    {
-       if(a[i][i]!=0)    /* Diagonal element is not zero */
-     for(j=0;j<r1;j++)
-       if (j!=i)
-       {
-          /* Make all the element above and nelow the current principal
-         diagonal element zero */
-          ratio = a[j][i]/a[i][i];
-          for(k=0;k<c1;k++)
-         a[j][k]-=ratio*a[i][k];
-       }
-       else
-         printf("\n");
-         /* principal Diagonal element is zero */
-       else
-       {
-       for(j=i+1;j<r1;j++)
-          if(a[j][i]!=0)
-          {   /* Find non zero elements in the same column */
-          swap(i,j,c1,a);
-          break;
-          }
-          if(j==r1)
-          {
-          c1--;
-          for(j=0;j<r1;j++)
-             a[j][i] = a[j][c1];
-          }
-          i--;
-         }
-     }
-   return  c1;
-}
+
 
 void save(double **a, int rows, int cols) {
   FILE *fp;
-  fp = fopen("result.dat", "w");
+  fp = fopen("m.dat", "w");
   //fprintf(fp, "The result of the multiplication is:\n");
   int i, j;
   for (i = 0; i < rows; i++) {
@@ -292,9 +214,5 @@ void save(double **a, int rows, int cols) {
   fclose(fp);
 }
 
-void save_r(int r)
-{
-	FILE *fp;                                   fp = fopen("rank.dat", "w");
-	fprintf(fp,"%d",r);
-fclose(fp);
-}
+
+
