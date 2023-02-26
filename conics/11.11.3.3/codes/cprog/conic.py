317e9cc1215877b8dd9cb6c4da1fc2f80f14ab46
likhith1101 <68158356+likhith1101@users.noreply.github.com>
2023-02-26 23:04:11 +0530

    Add files via upload

---
+++ conics/11.11.3.3/codes/cprog/conic.py
@@ -0,0 +1,109 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import linalg as LA
+from math import *
+import matplotlib.cm as cm
+import matplotlib.legend as Legend
+
+
+#end if
+def ellipse_gen(a,b):
+	len = 50
+	theta = np.linspace(0,2*np.pi,len)
+	x_ellipse = np.zeros((2,len))
+	x_ellipse[0,:] = a*np.cos(theta)
+	x_ellipse[1,:] = b*np.sin(theta)
+	return x_ellipse
+
+def line_gen(A,B):
+  len =10
+  dim = A.shape[0]
+  x_AB = np.zeros((dim,len))
+  lam_1 = np.linspace(0,1,len)
+  for i in range(len):
+    temp1 = A + lam_1[i]*(B-A)
+    x_AB[:,i]= temp1.T
+  return x_AB
+
+def affine_transform(P,c,x):
+    return P@x + c
+
+#Input parameters
+
+
+
+#Input parameters
+a=4
+b=3
+ellipAxis_A= np.loadtxt('ellipAxis_A.dat',dtype='double')
+ellipAxis_B= np.loadtxt('ellipAxis_B.dat',dtype='double')
+ellipMinorAxis_A=np.loadtxt('ellipMinorAxis_A.dat',dtype='float')
+ellipMinorAxis_B=np.loadtxt('ellipMinorAxis_B.dat',dtype='float')
+A=np.loadtxt('A.dat',dtype='float')
+B=np.loadtxt('B.dat',dtype='float')
+C=np.loadtxt('C.dat',dtype='float')
+D=np.loadtxt('D.dat',dtype='float')
+H=np.loadtxt('H.dat',dtype='float')
+G=np.loadtxt('G.dat',dtype='float')
+F1=np.loadtxt('F1.dat',dtype='float')
+F2=np.loadtxt('F2.dat',dtype='float')
+
+
+#Generating the ellipse
+elli=  ellipse_gen(a,b) 
+
+# Generating lines 
+x_AB = line_gen(ellipAxis_A, ellipAxis_B)
+x_minor_AB = line_gen(ellipMinorAxis_A, ellipMinorAxis_B)
+x_lr1_AB = line_gen(A , B )
+x_lr2_CD = line_gen(C , D )
+
+#Plotting the ellipse
+plt.plot(elli[0,:],elli[1,:],label='$Ellipse$')
+
+leg_label = "{} {}".format("Major", "Axis")
+plt.plot(x_AB[0,:],x_AB[1,:] ,label = leg_label)
+
+leg_label = "{} {}".format("Minor", "Axis")
+plt.plot(x_minor_AB[0,:],x_minor_AB[1,:] ,label = leg_label)
+
+leg_label = "{} {}".format("Latus", "Rectum1")
+plt.plot(x_lr1_AB[0,:],x_lr1_AB[1,:] ,label = leg_label)
+
+leg_label = "{} {}".format("Latus", "Rectum2")
+plt.plot(x_lr2_CD[0,:],x_lr2_CD[1,:] ,label = leg_label)
+
+
+#Labeling the coordinates
+plot_coords = np.vstack((F1,F2,H,G)).T
+vert_labels = ['$F_1$','$F_2$','$V_1$','$V_2$']
+for i, txt in enumerate(vert_labels):
+    if ( i == 0) :
+      label = "{}".format('$F_1 - Focus 1$' )
+    elif ( i == 1) :
+      label = "{}".format('$F_2 - Focus 2$' )
+    elif ( i == 2) :
+      label = "{}".format('$V_1 - Vertex 1$' )
+    else :
+      label = "{}".format('$V_2 - Vertex 2$' )
+
+    plt.scatter(plot_coords[0,i], plot_coords[1,i], s=15, label = label)
+    plt.annotate(txt, # this is the text
+                (plot_coords[0,i], plot_coords[1,i]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(8,5), # distance from text to points (x,y)
+                 fontsize=7,
+                 ha='center') # horizontal alignment can be left, right or center
+
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+
+plt.gca().legend(loc='lower left', prop={'size':6},bbox_to_anchor=(0.93,0.6))
+plt.grid() # minor
+
+plt.axis('equal')
+plt.title('Ellipse')
+#if using termux
+#plt.savefig('../figs/fig.pdf')
+#else
+plt.show()
---
+++ conics/11.11.3.3/codes/cprog/main.c
@@ -0,0 +1,83 @@
+/*Code by G. Arun Kumar (works on termux)
+Feb 21, 2023
+Link to License("https://github.com/aruniot099/FWC-1/blob/main/License")
+Find the coordinates of the focii, the vertices, the length of major and minor axes, the eccentricity and the length of the latus rectum of an ellipse whose equation is given by x^2/36 +y^2/16 = 1.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include"lib.h"
+
+int main()
+{
+	// Declaring the variables
+	double **v, **u, **o, **z, **A, **B, **C, **D, **F1, **F2, **G, **H, **I, **J, **N, **M, **e1;
+	double **ellipAxis_A, **ellipAxis_B, **ellipMinorAxis_A, **ellipMinorAxis_B; 
+	double lamda0, lamda1, fl1, fl2, lr1_Ay, lr2_Ay;
+	int m=2,n=1, a=4, b=3, f=-144, f0=144;
+
+	// Loading the matrices from text files
+	v = loadtxt("v.dat", m, m);
+	u = loadtxt("u.dat", m, n);
+	o = loadtxt("o.dat", m, n);
+	N = loadtxt("n.dat", m, n);
+	e1 = loadtxt("e1.dat", m, n);
+	z = loadtxt("z.dat", m, n);
+
+	// Creating the matrices
+	G = np_array(a, 0);
+	H = np_array(-(a), 0);
+	I = np_array(0, b);
+	J = np_array(0, -(b));
+
+
+	// Determining the Lamda vlaues from V matrix
+	lamda0 = v[0][0];
+	lamda1 = v[1][1];
+
+        // Computing the eccentricity of ellipse
+    	double e = sqrt(1 - lamda0 / lamda1);
+    	double e_square = e * e;
+   	printf("Eccentricity of Ellipse is %lf\n", e);
+
+    	// Computing the Focal points 
+    	F1 = mult_float(e * sqrt( f0 / (lamda1 * (1 - pow(e, 2)))), e1, m, n);
+    	F2 = neg(F1, m, n);
+
+    	// Finding the Norm values of Focal Points
+    	fl1 = linalg_norm(F1, m);
+    	fl2 = linalg_norm(F2, m);
+
+    	// Determining the Directional vector
+    	M = matmul(z, N, m, n, 2);
+
+    	// Determining the points of Major Axis 
+    	ellipAxis_A = mult_float(1, N, m, n);
+    	ellipAxis_B = mult_float(-1, N, m, n);
+
+    	// Determinig the points of Minor Axis
+    	ellipMinorAxis_A=mult_float(1/1.33, M, m, n);
+    	ellipMinorAxis_B=mult_float(-1/1.33, M, m, n);
+
+    	lr1_Ay = sqrt((f0 - lamda0 * pow(fl1, 2)) / lamda1);
+    	A = linalg_add(F1, np_array(0, lr1_Ay), m, n);
+    	B = linalg_add(F1, np_array(0, -(lr1_Ay)), m, n);
+
+    	lr2_Ay = sqrt((f0 - lamda0 * pow(fl2,2)) / lamda1);
+    	C = linalg_add(F2, np_array(0, lr2_Ay), m, n);
+    	D = linalg_add(F2, np_array(0, -(lr2_Ay)), m, n);
+
+    	// Saving the results in text files
+    	save(ellipAxis_A, m, n,"ellipAxis_A.dat");
+    	save(ellipAxis_B, m, n, "ellipAxis_B.dat");
+    	save(ellipMinorAxis_A, m, n, "ellipMinorAxis_A.dat");
+    	save(ellipMinorAxis_B, m, n, "ellipMinorAxis_B.dat");
+    	save(A, m, n, "A.dat");
+    	save(B, m, n, "B.dat");
+    	save(C, m, n, "C.dat");
+    	save(D, m, n, "D.dat");
+    	save(H, m, n, "H.dat");
+    	save(G, m, n, "G.dat");
+    	save(F1, m, n, "F1.dat");
+    	save(F2, m, n, "F2.dat");
+}
